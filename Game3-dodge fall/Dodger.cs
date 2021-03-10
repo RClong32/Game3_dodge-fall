90a4186c0c6f0f58cf9f8e72787bf9c166bc5bc1
rclong <rclong@CIS-RDS3>
2021-03-09 21:55:38 -0600

    Add project files.

---
+++ Game3-dodge fall/Dodger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+//used the 32x32 bat sprite from opengameart.com
+
+namespace Game3_dodge_fall
+{
+    public class Dodger
+{
+     public enum Direction
+        {
+            Down = 0,
+            Right = 1,
+            Up = 2,
+            Left = 3,
+        }
+
+     private KeyboardState keyboardState;
+        //private KeyboardState previousKeyboardState;
+
+        private double animationTimer;
+
+        private short animationFrame = 1;
+
+        private Direction direction;
+
+
+        private Texture2D texture;
+
+        private Vector2 position;
+
+    public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("dodgerBat");
+
+        }
+
+    public void Update(GameTime gameTime)
+        {
+
+           
+            keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
+            {
+                direction = Direction.Up;
+                position += new Vector2(0, -2);
+                
+            }
+            if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S))
+            {
+                direction = Direction.Down;
+                position += new Vector2(0, 2);
+                
+            }
+            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
+            {
+                direction = Direction.Left;
+                position += new Vector2(-2, 0);
+                
+            }
+            if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+            {
+                direction = Direction.Right;
+                position += new Vector2(2, 0);
+                
+            }
+            }
+
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(animationTimer > 0.1)
+            {
+                animationFrame++;
+                if (animationFrame > 3) animationFrame = 1;
+                animationTimer -= 0.1;
+            }
+            var source = new Rectangle(animationFrame * 32, (int)direction * 32, 32, 32);
+            spriteBatch.Draw(texture, position, source, Color.White, 0, new Vector2(-75,-50), 4.0f, SpriteEffects.None, 0);
+        }
+
+    }
+}
---
+++ Game3-dodge fall/Game1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game3_dodge_fall
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Dodger dodger;
+        private Stabber stabber;
+        //private Texture2D at;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            dodger = new Dodger();
+            stabber = new Stabber();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            dodger.LoadContent(Content);
+            stabber.LoadContent(Content);
+            //at = Content.Load<Texture2D>("colored_packed");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            dodger.Update(gameTime);
+            stabber.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            dodger.Draw(gameTime, _spriteBatch);
+            stabber.Draw(gameTime, _spriteBatch);
+            //_spriteBatch.Draw(at, new Vector2(50, 50), new Rectangle(512, 112, 16, 16), Color.White, 2.3f, new Vector2(0,0), 2f, SpriteEffects.None,0);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
